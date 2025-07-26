c095462786f19b25880ef8bab4b6244677003894
Pablo Vasta <pablo@zoddak.com>
2025-07-26 13:30:54 +0200

    Añado dominio a cors

--- app/main.py
+++ app/main.py
@@ -10,7 +10,8 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",  # React dev
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    'http://vinnievasta.com'
 ]
 
 app.add_middleware(
