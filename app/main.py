0e09976d7cf5266dd7e4a2da342d2e500c79c3b8
Pablo Vasta <pablo@zoddak.com>
2026-05-26 10:18:57 +0200

    cors2

--- app/main.py
+++ app/main.py
@@ -6,28 +6,31 @@ from starlette.middleware.sessions import SessionMiddleware
 import os
 from fastapi.middleware.cors import CORSMiddleware
 
+load_dotenv()
+secret_key = os.getenv('CLAVESECRETA')
+
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",               
-    "http://127.0.0.1:3000",               
-    "http://localhost:5174",             
-    "https://vinnievasta.com",           
-    "https://www.vinnievasta.com",        
-    "https://backend-portfolio-iqkc.onrender.com",  
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5174",
+    "https://vinnievasta.com",
+    "https://www.vinnievasta.com",
+    "https://backend-portfolio-iqkc.onrender.com",
 ]
+
+# SessionMiddleware debe añadirse antes que CORSMiddleware
+# porque Starlette ejecuta los middlewares en orden inverso al que se añaden
+app.add_middleware(SessionMiddleware, secret_key=secret_key)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-load_dotenv()
-secret_key = os.getenv('CLAVESECRETA')
-
-app.add_middleware(SessionMiddleware, secret_key=secret_key)
 app.include_router(apis_router)
 app.include_router(education_router)
 app.include_router(experience_router)
